4bcffd97693ca4525084df249d7b9d14d142dee2
doniahmad <doniahmaddarmawan@gmail.com>
2022-11-08 19:14:27 +0700

    feat: [DA] create api auth

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // reset chaced roles and permission
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $arrayPremissionName = [
+            // product
+            "access product",
+            // admin
+            "access admin", "manage admin",
+            // owner
+            'access owner', 'manage owner',
+        ];
+
+        $premission = collect($arrayPremissionName)->map(function ($premission) {
+            return ['name' => $premission, 'guard_name' => "web"];
+        });
+
+        Permission::insert($premission->toArray());
+
+        // create role
+        Role::create(['name' => 'owner'])->givePermissionTo($arrayPremissionName);
+        Role::create(['name' => 'admin'])->givePermissionTo('access product', 'manage admin');
+        Role::create(['name' => 'customer'])->givePermissionTo('access product');
+    }
+}
