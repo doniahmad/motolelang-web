b583b834ad6b802f762ea782c82ac43a3f6c224c
ArilPopon <arilponconugroho@gmail.com>
2022-12-02 07:46:06 +0700

    feat: [AP] create ui for home,login,lelang,nvabar,footer,about

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.master')
+@section('title', 'About Us')
+
+<div class="container konten">
+    <div class="container ps-5">
+        <h5>Tentang Kami</h5>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="mt-4 ps-5">
+                <h4 class="mb-3">Apasih MOTO Lelang?</h4>
+                <p>
+                    MOTO Lelang adalah balai lelang sepeda motor yang memiliki visi untuk menjadi wadah
+                    jual beli kendaraan paling bisa diandalkan di Indonesia.
+                </p>
+                <p>
+                    Kami selalu menjamin keamanan barang lelang dan juga keaslian barang lelang. Hal itu
+                    bertujuan agar proses pelelangan berjalan lancar dan keamanan bagi pembeli (bidder) terjamin.
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="/assets/img/tanya_result.webp" alt="why?" width="80%">
+        </div>
+    </div>
+</div>
+
+<div class="container mt-4 ps-5">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="/assets/img/mockuplogo_result.webp" alt="mockup logo" width="80%">
+        </div>
+        <div class="col-md-6">
+            <div class="mt-4">
+                <h4 class="mb-3">Visi</h4>
+                <p>
+                    Menjadi Pelelangan Motor Online terdepan, terpercaya, dan terbaik di Indonesia
+                </p>
+                <h4 class="mb-3">Misi</h4>
+                <p>
+                    Memberikan pelayanan prima kepada seluruh pengguna jasa lelang. Menjadikan MOTO Lelang
+                    sebagai alternatif terbaik dalam menjual aset dan memaksimalkan nilai jual aset bagi klien
+                    MOTO Lelang.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5 ps-5">
+    <h4 class="text-center">Kenapa Harus MOTO Lelang?</h4>
+    <div class="container d-flex justify-content-center">
+        <div class="row mt-5 img-center text-center">
+            <div class="col-4">
+                <img src="/assets/img/cashmoney.svg" alt="cash money" width="30%">
+                <p class="mt-3">Harga Yang Bersahabat</p>
+            </div>
+            <div class="col-4">
+                <img src="/assets/img/fast.svg" alt="cepat" width="30%">
+                <p class="mt-3">Meningkatkan Efisiensi,Hemat Waktu, dan Biaya</p>
+            </div>
+            <div class="col-4">
+                <img src="/assets/img/variatif.svg" alt="Variatif" width="30%">
+                <p class="mt-3">Produk Yang Bervariatif</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-center">
+        <div class="row mt-3 img-center text-center">
+            <div class="col-4">
+                <img src="/assets/img/medal.svg" alt="Berkualitas" width="30%">
+                <p class="mt-3">Barang Berkualitas</p>
+            </div>
+            <div class="col-4">
+                <img src="/assets/img/document.svg" alt="Dokumen" width="30%">
+                <p class="mt-3">Dokumen Kendaraan Dijamin Lengkap</p>
+            </div>
+            <div class="col-4">
+                <img src="/assets/img/relax.svg" alt="Sans" width="30%">
+                <p class="mt-3">Bisa Ikut Lelang Dimana Saja dan Kapan Saja</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5  mb-5">
+        <h4 class="text-center">Apa Yang Pengguna Moto Lelang Katakan</h4>
+        <div class="d-flex justify-content-between">
+            <div class="card mt-5" style="width: 16rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <p>Saya Senang Ikut Pelelangan Motor Secara Online di MOTO Lelang dikarenakan harga
+                            yang bersahabat</p>
+                        <img src="/assets/img/avatar.svg" alt="" srcset="">
+                        <h5 class="mt-3">Indra Adi</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mt-5" style="width: 16rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <p>Saya Ikut Pelelangan Motor Online di MOTO Lelang Karena Dokumen Mengenai Motor Yang
+                            Dilelang Lengkap</p>
+                        <img src="/assets/img/avatar.svg" alt="" srcset="">
+                        <h5 class="mt-3">Heru</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mt-5" style="width: 16rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <p>Puas Banget Karena Saya Dapat Motor Yang Saya Inginkan Dengan Harga Yang Pas Di Kantong
+                            Saya xixixi</p>
+                        <img src="/assets/img/avatar.svg" alt="" srcset="">
+                        <h5 class="mt-3">Enggar</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mt-5" style="width: 16rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <p>Disuruh Emak Cari Motor Bekas Buat Adek Sekolah eh Dapat Motor Yang Sesuai Dengan Budget
+                            Saya Di MOTO Lelang</p>
+                        <img src="/assets/img/avatar.svg" alt="" srcset="">
+                        <h5 class="mt-3">Rian</h5>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+</div>
